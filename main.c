987c0dac31cc284c9a8ee32cf4448f972fde0199
Ashish Kushwaha <ashish_kushwaha@outlook.com>
2016-04-09 20:57:25 +0530

    Update main.c

--- main.c
+++ main.c
@@ -14,11 +14,11 @@ int main(int argc, char **argv) {
 	SPI_SlaveInit();
 	char a=0;
 	while(1){
-		a=SPI_SlaveReceive();
-		ADMUX=(0b11000000|a);
-		ADCSRA=0b11000000;
-		while(!(ADCSRA&(1<<ADIF)));
-		SPDR=(ADC/4);
+		a=SPI_SlaveReceive(); //waiting for reception 
+		ADMUX=(0b11000000|a); //selecting channels 
+		ADCSRA=0b11000000;    //triggering th ADC module
+		while(!(ADCSRA&(1<<ADIF))); //waiting for the complition
+		SPDR=(ADC/4);           //scaling to 8 bit
 	}
 }
 
